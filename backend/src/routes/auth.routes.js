6c2eecf9816d10dc0451ef8f4dc33d38631a3a65
Shah Zaib <125696406+Shah039zaib@users.noreply.github.com>
2025-12-10 20:14:55 +0500

    Update auth.routes.js

--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -1,9 +1,11 @@
 // backend/src/routes/auth.routes.js
 const express = require('express');
 const router = express.Router();
-const { register, login } = require('../controllers/auth.controller');
+const { register, login, me } = require('../controllers/auth.controller');
+const { authMiddleware } = require('../middlewares/auth.middleware');
 
 router.post('/register', register);
 router.post('/login', login);
+router.get('/me', authMiddleware, me);
 
 module.exports = router;
